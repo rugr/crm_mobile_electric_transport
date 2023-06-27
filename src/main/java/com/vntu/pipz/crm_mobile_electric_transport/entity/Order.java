3de32281bb6c61827e5ceeddc59aa3621435176a
Руслан <ru_gr@ukr.net>
2023-06-28 00:16:11 +0300

    add: entity

---
+++ src/main/java/com/vntu/pipz/crm_mobile_electric_transport/entity/Order.java
@@ -0,0 +1,57 @@
+package com.vntu.pipz.crm_mobile_electric_transport.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [" +
+                "id=" + id +
+                ", orderItems=" + orderItems +
+                ", user=" + user.getUsername() +
+                ']';
+    }
+}
