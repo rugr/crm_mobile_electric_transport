65e6fe90334ca1b7e7a7fd689f899eeca166124b
Руслан <ru_gr@ukr.net>
2023-12-11 23:31:36 +0200

    add UserServiceTest

---
+++ src/test/java/com/vntu/pipz/crm_mobile_electric_transport/service/user/UserServiceTest.java
@@ -0,0 +1,32 @@
+package com.vntu.pipz.crm_mobile_electric_transport.service.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void allUsers() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void usergtList() {
+    }
+}
