6201a8f0203f067fec23e0516c121fd141f7dac4
Руслан <ru_gr@ukr.net>
2023-12-14 23:57:44 +0200

    add fields to Product

--- src/main/java/com/vntu/pipz/crm_mobile_electric_transport/entity/Product.java
+++ src/main/java/com/vntu/pipz/crm_mobile_electric_transport/entity/Product.java
@@ -17,12 +17,28 @@ public class Product {
     @Column(name = "price")
     private int price;
 
+    @Column(name = "power")
+    private int power;
+
+    @Column(name = "battery_energy")
+    private int batteryEnergy;
+
+    @Column(name = "range")
+    private int range;
+
+    @Column(name = "speed")
+    private int speed;
+
     public Product() {}
 
-    public Product(Long id, String title, int price) {
+    public Product(Long id, String title, int price, int power, int batteryEnergy, int range, int speed) {
         this.id = id;
         this.title = title;
         this.price = price;
+        this.power = power;
+        this.batteryEnergy = batteryEnergy;
+        this.range = range;
+        this.speed = speed;
     }
 
     public Long getId() {
@@ -49,12 +65,48 @@ public class Product {
         this.price = price;
     }
 
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getBatteryEnergy() {
+        return batteryEnergy;
+    }
+
+    public void setBatteryEnergy(int batteryEnergy) {
+        this.batteryEnergy = batteryEnergy;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
     @Override
     public String toString() {
-        return "Product [" +
+        return "Product{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", price=" + price +
-                ']';
+                ", power=" + power +
+                ", batteryEnergy=" + batteryEnergy +
+                ", range=" + range +
+                ", speed=" + speed +
+                '}';
     }
 }
